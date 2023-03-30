0b2f0183f31b21ddd9300d5cdadac96c1c7be03b
Kristoffer Ahlstrom-Vij <ahlstromvij@Kristoffers-MacBook-Pro.local>
2023-03-30 16:36:49 +0100

    Add additional knowledge variables and CFA to scales script

--- R/03_scales.R
+++ R/03_scales.R
@@ -2,8 +2,9 @@ set.seed(100)
 
 library(tidyverse)
 library(informationeffects)
-
-lapply(df_all_years, class)
+library(polycor)
+library(psych)
+library(lavaan)
 
 df_all_years <- readRDS("data/df_all_years_imputed.rds")
 
@@ -11,6 +12,8 @@ df_all_years <- readRDS("data/df_all_years_imputed.rds")
 df_all_years <- df_all_years %>% 
   mutate(k_m_rep = as.numeric(k_m_rep),
          k_s_rep = as.numeric(k_s_rep),
+         k_c_rep = as.numeric(k_c_rep),
+         k_v_rep = as.numeric(k_v_rep),
          k_nat_insurance = as.numeric(k_nat_insurance),
          k_num_mps = as.numeric(k_num_mps),
          k_spain_eu = as.numeric(k_spain_eu))
@@ -64,9 +67,21 @@ df_all_years %>%
   coord_flip()
 
 # knowledge scale
-know_scale <- info_scale(items = c("k_s_rep","k_num_mps","k_spain_eu"),
+know_scale <- info_scale(items = c("k_s_rep",
+                                   "k_m_rep",
+                                   "k_c_rep",
+                                   "k_v_rep",
+                                   "k_nat_insurance",
+                                   "k_num_mps",
+                                   "k_spain_eu"),
                          data = df_all_years,
-                         binary_cutoff = 1)
+                         binary_cutoff = 0.75)
+
+know_scale$know_scores %>% 
+  as.data.frame() %>% 
+  ggplot() +
+  aes(x = know_scale$know_scores) +
+  geom_histogram(bins = 7, fill = 'salmon', color = 'black')
 
 know_scale$know_scores_binary_tbl
 know_scale$model_summary
@@ -76,6 +91,39 @@ know_scale$trace_plot
 know_scale$info_plot
 know_scale$q3
 know_scale$empirical_plots
+
+# let's look at these with efa, using polychoric correlations
+efa_poly <- fa.poly(df_all_years[,8:14], 2)
+print(efa_poly$fa$loadings)
+
+# let's compare fit of two models using CFA
+# one factor
+mod_1f <- 'knowledge =~ k_s_rep + k_m_rep + k_c_rep + k_v_rep + k_nat_insurance + k_num_mps + k_spain_eu'
+mod_1f.fit <- cfa(mod_1f, data=df_all_years, ordered = TRUE)
+summary(mod_1f.fit, standardized=TRUE)
+# the Std.all column gives the type of loadings we would see in efa correlations between items and its latent. we want > 0.3.
+
+# RMSEA values less than 0.05 or 0.01 correspond to good and very good fit respectively (Andrews 2021)
+# want Comparative Fit Index (CFI) >0.95 (Dima 2018)
+# want Tucker-Lewis index (TLI) >0.95 (Dima 2018)
+# agfi: adjusted goodness of fit; 0-1, higher better
+
+fitmeasures(mod_1f.fit)["rmsea"] # 0.08119324
+fitmeasures(mod_1f.fit)["cfi"] # 0.9593425
+fitmeasures(mod_1f.fit)["tli"] # 0.9390138
+fitmeasures(mod_1f.fit)["agfi"] # 0.9546318
+
+# two factors
+mod_2f <- '
+          person_knowledge =~ k_s_rep + k_m_rep + k_c_rep + k_v_rep
+          issue_knowledge =~ k_nat_insurance + k_num_mps + k_spain_eu'
+mod_2f.fit <- cfa(mod_2f, data=df_all_years, ordered = TRUE)
+summary(mod_2f.fit, standardized=TRUE)
+
+fitmeasures(mod_2f.fit)["rmsea"] # 0.06153559
+fitmeasures(mod_2f.fit)["cfi"] # 0.9783145
+fitmeasures(mod_2f.fit)["tli"] # 0.9649696
+fitmeasures(mod_2f.fit)["agfi"] # 0.9737486
 
 # construct validity
 df_all_years$knowledge <- know_scale$know_scores
@@ -138,7 +186,7 @@ dev.off()
 # calculate prop scores
 df_all_years$knowledge_binary <- know_scale$know_scores_binary
 df_all_years$prop_score <- info_prop_scores(knowledge_var = "knowledge_binary", 
-                                            covariates = c("d_income","d_education","d_gender","d_age","d_class"), 
+                                            covariates = c("d_income","d_education","d_gender","d_age"), 
                                             data = df_all_years)
 
 df_all_years %>% 
@@ -148,7 +196,7 @@ df_all_years %>%
 
 # evaluate prop scores
 info_bal_plots(knowledge_var = "knowledge_binary", 
-               covariates = c("d_income","d_education","d_gender","d_age","d_class"), 
+               covariates = c("d_income","d_education","d_gender","d_age"), 
                prop_score ="prop_score", 
                data = df_all_years)
 
